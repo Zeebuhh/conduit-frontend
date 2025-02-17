a1244eecad90075786e655565e0a263dfeda030b
Zeebuhh <ngroth277@gmail.com>
2025-02-17 20:52:11 +0100

    testing submodule

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
+declare const process: {
+  env: {
+    API_URL: string;
+  };
+};
+
 export const environment = {
   production: false,
-  api_url: "http://localhost:8000/api",
+  api_url: process.env.API_URL || "http://localhost:8000/api",
 };
